4d4218a5fdeaa67d3e3d7f4afe5faf2decfe8f1b
bryan@tomlin.email <bryan@tomlin.email>
2026-05-27 15:57:29 -0500

    better running in terminal

--- nvim/plugin/rust.lua
+++ nvim/plugin/rust.lua
@@ -1,3 +1,6 @@
 vim.pack.add({
-  "https://github.com/rust-lang/rust.vim"
+  "https://github.com/rust-lang/rust.vim",
 })
+
+vim.keymap.set("n", "<leader>rr", ":Cargo run<CR>", { desc = "Cargo Run Rust code" })
+vim.keymap.set("n", "<leader>rt", ":Cargo test<CR>", { desc = "Cargo Run Rust tests" })
