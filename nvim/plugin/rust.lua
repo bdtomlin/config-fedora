db714cf7697518d7b24e5fbeef0998a39c6fe349
bryan@tomlin.email <bryan@tomlin.email>
2026-06-03 17:36:16 -0500

    vim rust key bindings

--- nvim/plugin/rust.lua
+++ nvim/plugin/rust.lua
@@ -2,5 +2,5 @@ vim.pack.add({
   "https://github.com/rust-lang/rust.vim",
 })
 
-vim.keymap.set("n", "<leader>rr", ":Cargo run<CR>", { desc = "Cargo Run Rust code" })
-vim.keymap.set("n", "<leader>rt", ":Cargo test<CR>", { desc = "Cargo Run Rust tests" })
+vim.keymap.set("n", "<leader>rr", ":Cargo run<CR>G", { desc = "Cargo Run Rust code" })
+vim.keymap.set("n", "<leader>rt", ":Cargo test<CR>G", { desc = "Cargo Run Rust tests" })
